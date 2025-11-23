0f1c989f5e7768706f1e5ec018451945bce9db2c
Ahmed <ahmedehab1677@gmail.com>
2025-11-24 01:29:54 +0200

    Add Hero section to App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 
 const App = () => {
   return (
     <div className="relative w-screen min-h-screen overflow-x-auto">
       <Navbar />
+      <Hero />
     </div>
   );
 };
